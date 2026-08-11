4d08dea79eb2a5b7b180c0b5256ed3f7b418fa8d
Gurneet Singh Saini <gurneetsaini53@gmail.com>
2026-08-10 23:47:42 -0400

    Final Lab8

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const { MongoStore } = require('connect-mongo');
 const { body, validationResult } = require('express-validator');
 
 require('dotenv').config();
